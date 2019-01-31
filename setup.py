f9081c6472c88615c4a0afaf52246c3b301c1f43
Vivek Joshy <daegontaven@gmail.com>
2019-01-31 17:30:58 -0500

    Try [voice] again

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ EXTRAS_REQUIRE = {
         'sphinx_rtd_theme'
     ],
     'voice': [
-        "discord.py @ git+https://github.com/Rapptz/discord.py.git@rewrite#egg=discord.py[voice]"
+        "discord.py [voice] @ git+https://github.com/Rapptz/discord.py.git@rewrite#egg=discord.py"
     ]
 }
 
