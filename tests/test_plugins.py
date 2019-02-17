9242a343b49f8c638ce5ca28d1f5c5debecb920b
Vivek Joshy <daegontaven@gmail.com>
2019-02-17 00:21:46 -0500

    Add plugin_data import tests

--- tests/test_plugins.py
+++ tests/test_plugins.py
@@ -4,7 +4,7 @@ from tests import fake_plugins
 
 import pytest
 from glia.core.exceptions import PluginNotFoundError
-from glia.core.utils import find_plugins
+from glia.core.utils import find_plugins, get_plugin_data
 
 
 def test_find_plugins():
@@ -31,3 +31,16 @@ def test_find_plugins():
 
     with pytest.raises(TypeError) as e_info:
         find_plugins(123)
+
+
+def test_get_plugin_data():
+    """
+    This is used to test if plugin data is imported correctly.
+    """
+    plugins_data_dict = {
+        'fake_plugins.core': {
+            "name": "Test Core Plugins"
+        }
+    }
+    for plugin, data in plugins_data_dict.items():
+        assert get_plugin_data(plugin)["name"] == data["name"]
