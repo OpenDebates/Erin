a29cf8953056b45955bd698236be32070d33d1f9
Vivek Joshy <daegontaven@gmail.com>
2019-04-02 12:26:16 +0530

    Fix install failing due to setup.cfg

--- erin/__main__.py
+++ erin/__main__.py
@@ -1,9 +1,23 @@
 import argparse
+import logging
 import sys
+
+from verboselogs import VerboseLogger
 
 import erin
 from erin.cli.scaffold import ScaffoldCommand
 from erin.cli.start import StartCommand
+
+# Constants
+LOG_FORMAT = "%(asctime)s %(name)-18s %(levelname)-8s %(message)s"
+DATE_FORMAT = "[%Y-%m-%d %H:%M:%S %z]"
+
+# Set Library Logging Formats
+logging.setLoggerClass(VerboseLogger)
+stream_handler = logging.StreamHandler(sys.stdout)
+stream_handler.setFormatter(logging.Formatter(LOG_FORMAT, DATE_FORMAT))
+root_logger = logging.getLogger(__name__)
+root_logger.addHandler(stream_handler)
 
 
 def _optional_commands(parser):
