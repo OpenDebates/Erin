3430e22c0eef0078059ebfaf840bec2dec082a5c
Taven Argus <daegontaven@gmail.com>
2019-11-28 15:02:38 +0530

    Closes #18

--- erin/cli/scaffold.py
+++ erin/cli/scaffold.py
@@ -9,4 +9,26 @@ class ScaffoldCommand(CommandFactory):
         self.parser.set_defaults(action=self.run)
 
     def run(self, *sys_args, **kwargs):
-        cookiecutter("https://github.com/DiscordFederation/cookiecutter-erin.git")
+        print("Enter the details for your project below!")
+        extra_context = {
+            "project_name": ("Project Name", "Bot"),
+            "project_slug": ("Project Slug", "bot"),
+            "version": ("Version Number", "0.0.0.dev0"),
+            "config_file": ("Configuration File Name", "config.toml"),
+            "_copy_without_render": [
+                "plugins/*"
+            ]
+        }
+        for key, val in extra_context.items():
+            if isinstance(val, tuple):
+                setting = input(f"{val[0]} [{val[1]}]: ")
+                if len(setting.strip()) == 0:
+                    extra_context[key] = val[1]
+                else:
+                    extra_context[key] = setting
+
+        cookiecutter(
+            "https://github.com/DiscordFederation/cookiecutter-erin.git",
+            no_input=True,
+            extra_context=extra_context
+        )
