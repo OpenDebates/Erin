55fdebd0f6d0dd4a9d75a5b9c421862eafc93de2
taven <8206808+daegontaven@users.noreply.github.com>
2021-02-02 11:48:43 +0530

    Bump dev version

--- erin/__init__.py
+++ erin/__init__.py
@@ -5,7 +5,7 @@ import sys
 from verboselogs import VerboseLogger
 
 dist_name = "Erin"
-__version__ = "0.1.0.dev4"
+__version__ = "0.1.0.dev5"
 
 # Constants
 LOG_FORMAT = "%(asctime)s %(name)-18s %(levelname)-8s %(message)s"
